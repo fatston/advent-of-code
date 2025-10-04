f13587366967829e0a77a1a8b283bb56353b9e0b
fatston <cliftonkor@gmail.com>
2025-10-04 22:51:30 +0800

    solved day2

--- src/solutions/Day2.java
+++ src/solutions/Day2.java
@@ -2,6 +2,7 @@ package solutions;
 
 import helper.FileReaderUtil;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -13,14 +14,7 @@ public class Day2 {
         var data = FileReaderUtil.readFileToList(path);
         var safeCount = 0;
         for (var list : data) {
-            if (list.get(0).equals(list.get(1))) {
-                continue; // unsafe
-            }
-            if (list.get(0) > list.get(1)) {
-                Collections.reverse(list);
-            }
-            // now should be all increasing
-            if (isSafeIncreasing(list)) {
+            if (isSafe(list)) {
                 safeCount += 1;
             }
         }
@@ -31,36 +25,26 @@ public class Day2 {
         var data = FileReaderUtil.readFileToList(path);
         var safeCount = 0;
         for (var list : data) {
-            if (list.get(0).equals(list.get(1))) {
-                if (list.get(1) > list.get(2)) {
-                    Collections.reverse(list);
+            for (int i = 0; i < list.size(); i++) {
+                var tempList = new ArrayList<>(list);
+                tempList.remove(i);
+                if (isSafe(tempList)) {
+                    safeCount++;
+                    break;
                 }
-            } else if (list.get(0) > list.get(1)) {
-                Collections.reverse(list);
-            }
-            // now should be all increasing
-            if (isLenientlySafeIncreasing(list)) {
-                safeCount += 1;
             }
         }
         return safeCount;
     }
 
-    public boolean isLenientlySafeIncreasing(List<Integer> l) {
-        var prev = l.get(0);
-        var isChanceUsed = false;
-        for (int i = 1; i < l.size(); i++) {
-            var curr = l.get(i);
-            if (prev >= curr || Math.abs(prev-curr) > 3) {
-                if (isChanceUsed) {
-                    return false;
-                } else {
-                    isChanceUsed = true;
-                }
-            }
-            prev = curr;
+    private Boolean isSafe(List<Integer> list) {
+        if (list.get(0).equals(list.get(1))) {
+            return false;
         }
-        return true;
+        if (list.get(0) > list.get(1)) {
+            Collections.reverse(list);
+        }
+        return isSafeIncreasing(list);
     }
 
     public boolean isSafeIncreasing(List<Integer> l) {
