5acebd2ea62a297f4f7bcb5ed3734d93b96f64e6
fatston <cliftonkor@gmail.com>
2025-10-11 23:18:32 +0800

    completed day4 part1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import solutions.Day1;
 import solutions.Day2;
 import solutions.Day3;
+import solutions.Day4;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -17,6 +18,11 @@ public class Main {
         var day3 = new Day3("src/input/day3.txt");
         System.out.println("day 3 part 1: " + day3.partOne());
         System.out.println("day 3 part 2: " + day3.partTwo());
+
+        var day4Test = new Day4("src/input/day4_test.txt");
+        System.out.println("day 4 part 1 test: " + day4Test.partOne() + " should be 18");
+        var day4 = new Day4("src/input/day4.txt");
+        System.out.println("day 4 part 1: " + day4.partOne());
 
     }
 }
--- src/solutions/Day4.java
+++ src/solutions/Day4.java
@@ -2,30 +2,109 @@ package solutions;
 
 import helper.FileReaderUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Day4 {
     private List<String> input;
+    private Integer iMax;
+    private Integer jMax;
 
     public Day4(String path) {
         input = FileReaderUtil.readFileToStringList(path);
+        iMax = input.size();
+        jMax = input.get(0).length();
     }
 
     public Integer partOne() {
-        var n = input.size();
-        var m = input.get(0).length();
 
-        // get all diagonals as list of integers
-        // get downs as list of integers
+        var fullList = new ArrayList<String>();
+
+        // get diagonals
+        fullList.add(getDiagonal(0, 0, input));
+        for (int i = 1; i < iMax; i++) {
+            fullList.add(
+                    getDiagonal(i,0, input)
+            );
+        }
+        for (int j = 1; j < jMax; j++) {
+            fullList.add(
+                    getDiagonal(0, j, input)
+            );
+        }
+
+        // get reverse diagonals
+        fullList.add(getReverseDiagonal(iMax - 1, 0, input));
+        for (int j = 1; j < jMax; j++) {
+            fullList.add(getReverseDiagonal(iMax - 1, j, input));
+        }
+        for (int i = iMax - 2; i >= 0; i--) {
+            fullList.add(getReverseDiagonal(i, 0, input));
+        }
+
+
+        // get downs as list of chars
+        for (int j = 0; j < jMax; j++) {
+            fullList.add(
+                    getDown(j, input)
+            );
+        }
+        // get across
+        for (int i = 0; i < iMax; i++) {
+            fullList.add(
+                    input.get(i)
+            );
+        }
+
         // pass all lists into countXmases and return
+        var count = 0;
+        for (String s : fullList) {
+            count += countXmases(s);
+        }
+        return count;
+    }
 
-        return 0;
+    private String getAcross(Integer i, List<String> grid) {
+        return grid.get(i);
+    }
 
+    private String getDown(Integer j, List<String> grid) {
+        var sb = new StringBuilder();
+        for (int i = 0; i < iMax; i++) {
+            sb.append(
+                    grid.get(i).charAt(j)
+            );
+        }
+        return sb.toString();
+    }
+
+    private String getDiagonal(Integer i, Integer j, List<String> grid) {
+        var sb = new StringBuilder();
+        while (i < iMax && j < jMax) {
+            sb.append(
+                    grid.get(i).charAt(j)
+            );
+            i++;
+            j++;
+        }
+        return sb.toString();
+    }
+
+    private String getReverseDiagonal(Integer i, Integer j, List<String> grid) {
+        var sb = new StringBuilder();
+        while (i >= 0 && j < jMax) {
+            sb.append(
+                    grid.get(i).charAt(j)
+            );
+            i--;
+            j++;
+        }
+        return sb.toString();
     }
 
     private Integer countXmases(String s) {
-        var pattern = Pattern.compile("XMAS|SAMX");
+        var pattern = Pattern.compile("(?=(XMAS|SAMX))");
         var matcher = pattern.matcher(s);
         var count = 0;
         while (matcher.find()) count++;
